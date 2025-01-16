d19be100147dbfcb99b62f4b4cde99da16ee3d00
dhanarrizky <dhanarrizky23@gmail.com>
2025-01-16 19:58:04 +0700

    updated code for be and fe

--- BACKEND/BankTransaction.DataAccessAndBusiness/obj/Debug/net8.0/BankTransaction.DataAccessAndBusiness.AssemblyInfo.cs
+++ BACKEND/BankTransaction.DataAccessAndBusiness/obj/Debug/net8.0/BankTransaction.DataAccessAndBusiness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankTransaction.DataAccessAndBusiness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca54dbf6e2ce5951bf2d471b7a2fe16fe4566e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2819cfe58e410dcc7f2f6db8570cd96c495c0d73")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankTransaction.DataAccessAndBusiness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankTransaction.DataAccessAndBusiness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
